c7b442556ab2df2cb54d07a40173d157e9c19b1e
Dieg0Code <diegoobando20@gmail.com>
2023-12-30 03:24:19 -0300

    getting data from an api with http package

--- Dart/paquetes/bin/paquetes.dart
+++ Dart/paquetes/bin/paquetes.dart
@@ -1,11 +1,17 @@
 import 'package:http/http.dart' as http;
-import 'dart:convert' as convert;
+// import 'dart:convert' as convert;
+
+import 'classes/reqres_response.dart';
 // import 'package:paquetes/paquetes.dart' as paquetes;
 
 void main(List<String> arguments) {
   final url = Uri.parse('https://reqres.in/api/users?page=2');
 
   http.get(url).then((res) {
-    final body = convert.jsonDecode(res.body);
+    // final body = convert.jsonDecode(res.body);
+    final resReqRes = reqResResponseFromJson(res.body);
+    print('page: ${resReqRes.page}');
+    print('per_page: ${resReqRes.perPage}');
+    print('id del tercer elemento: ${resReqRes.data[2].id}');
   });
 }
